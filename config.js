43f753a3aa812acfc0230f01eb2b7ab32391b25c
chayentot <chayentot@gmail.com>
2026-07-14 13:50:40 +0800

    Update APP_CONFIG to use API_URL key

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.APP_CONFIG={https://script.google.com/macros/s/AKfycbw9zv-OSl3WqAFjr3dF-NlZ0Vf1Aep6Aup64kCFWSY1K1_QrLqZm9rxxn4cOf40TzNt/exec'};
+window.APP_CONFIG={API_URL:'https://script.google.com/macros/s/AKfycbw9zv-OSl3WqAFjr3dF-NlZ0Vf1Aep6Aup64kCFWSY1K1_QrLqZm9rxxn4cOf40TzNt/exec'};
